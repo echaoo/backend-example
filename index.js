18ae9637edf0568a6764519516839f43b3c67214
echaoo <1299529160@qq.com>
2017-12-13 23:22:32 +0800

    增加删除功能

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var db_connecter = require('./dbConfig')
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
 var comments = require('./comments')
 
 app.get('/', function (req, res) {
@@ -14,6 +17,10 @@ app.post('/postinfo', function (req, res, next) {
 
 app.get('/api/comments', function (req, res, next) {
     comments.getMessages(req, res, next);
+});
+
+app.post('/api/delete', function (req, res, next) {
+    comments.deleteMessage(req, res, next);
 });
 
 app.listen(3000, function () {
